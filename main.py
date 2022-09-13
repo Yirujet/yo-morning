9a14e582e6f729ab1f81af6e661e27adfc82494b
liyongjie <liyongjie@xwmail.com>
2022-09-13 09:52:53 +0800

    init project

---
+++ main.py
@@ -0,0 +1,66 @@
+from datetime import date, datetime
+import math
+from wechatpy import WeChatClient
+from wechatpy.client.api import WeChatMessage, WeChatTemplate
+import requests
+import os
+import random
+
+today = datetime.now()
+start_date = os.environ['START_DATE']
+city = os.environ['CITY']
+birthday = os.environ['BIRTHDAY']
+
+app_id = os.environ["APP_ID"]
+app_secret = os.environ["APP_SECRET"]
+
+user_id = os.environ["USER_ID"]
+template_id = os.environ["TEMPLATE_ID"]
+
+
+def get_weather():
+  url = "http://autodev.openspeech.cn/csp/api/v2.1/weather?openId=aiuicus&clientType=android&sign=android&city=" + city
+  res = requests.get(url).json()
+  return weather
+
+def get_count():
+  delta = today - datetime.strptime(start_date, "%Y-%m-%d")
+  return delta.days
+
+def get_birthday():
+  next = datetime.strptime(str(date.today().year) + "-" + birthday, "%Y-%m-%d")
+  if next < datetime.now():
+    next = next.replace(year=next.year + 1)
+  return (next - today).days
+
+def get_words():
+  words = requests.get("https://api.shadiao.pro/chp")
+  if words.status_code != 200:
+    return get_words()
+  return words.json()['data']['text']
+
+def get_random_color():
+  return "#%06x" % random.randint(0, 0xFFFFFF)
+
+
+client = WeChatClient(app_id, app_secret)
+
+wm = WeChatMessage(client)
+wea = get_weather()
+data = {
+    "love_days":{"value":get_count()},
+    "birthday_left":{"value":get_birthday()},
+    "words":{"value":get_words(), "color":get_random_color()},
+    "date": { "value": wea["date"] },
+    "city": { "value": city },
+    "temperature": { "value": math.floor(wea["temp"]) },
+    "weather": { "value": wea["weather"] },
+    "wind": { "value": wea["wind"] },
+    "airData": { "value": wea["airData"] },
+    "airQuality": { "value": wea["airQuality"] },
+    "highest": { value: math.floor(wea["high"]) },
+    "lowest": { value: math.floor(wea["low"]) },
+    "lastUpdateTime": { value: wea["lastUpdateTime"] }
+}
+res = wm.send_template(user_id, template_id, data)
+print(res)
