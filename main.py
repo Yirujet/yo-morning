d0d63e77655b0d23cd1cb23d1a01cef50c202cbf
liyongjie <liyongjie@xwmail.com>
2022-09-13 11:21:45 +0800

    refactor: 给关键信息加上颜色标记

--- main.py
+++ main.py
@@ -49,11 +49,11 @@ client = WeChatClient(app_id, app_secret)
 wm = WeChatMessage(client)
 wea = get_weather()
 data = {
-    "love_days":{"value":get_count()},
-    "birthday_left":{"value":get_birthday()},
+    "love_days":{"value":get_count(), "color":get_random_color()},
+    "birthday_left":{"value":get_birthday(), "color":get_random_color()},
     "words":{"value":get_words(), "color":get_random_color()},
     "date": { "value": wea["date"] },
-    "city": { "value": city },
+    "city": { "value": city, "color":get_random_color() },
     "temperature": { "value": math.floor(wea["temp"]) },
     "weather": { "value": wea["weather"] },
     "wind": { "value": wea["wind"] },
