5c8980619226331df366010e81dd93dc4fe8edd8
Yirujet <409273887@qq.com>
2023-02-10 10:14:54 +0800

    Update main.py

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ template_id = os.environ["TEMPLATE_ID"]
 def get_weather():
   url = "https://v0.yiketianqi.com/api?unescape=1&version=v91&appid=43656176&appsecret=I42og6Lm&ext=&cityid=&city=" + city
   res = requests.get(url).json()
-  weather = res.data.data[0]
+  weather = res
   return weather
 
 def get_count():
